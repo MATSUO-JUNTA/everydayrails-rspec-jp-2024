d2551985796b841f538f34c1b541fa427ae81abc
MATSUO-JUNTA <junta0527@icloud.com>
2025-02-16 22:18:46 +0900

    メソッドのテスト マッチャについて

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     user = User.create(
@@ -14,7 +13,7 @@ RSpec.describe Project, type: :model do
       name: "Test Project",
     )
 
-    new_project = user.projects(
+    new_project = user.projects.build(
       name: "Test Project"
     )
 
@@ -22,6 +21,7 @@ RSpec.describe Project, type: :model do
 
     expect(new_project.errors[:name]).to include("has already been taken")
   end
+  
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
     user = User.create(
@@ -41,7 +41,7 @@ RSpec.describe Project, type: :model do
       password:   "dottle-nouveau-pavilion-tights-furze",
     )
 
-    other_user.other_user.projects.build(
+    other_project = other_user.projects.build(
       name: "Test Project",
     )
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,5 +43,12 @@ RSpec.describe User, type: :model do
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address"
   # ユーザーのフルネームを文字列として返すこと
-  it "returns a user's full name as a string"
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@example.com",
+    )
+    expect(user.name).to eq "John Doe"
+  end
 end
